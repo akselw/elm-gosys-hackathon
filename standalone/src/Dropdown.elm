1a8f7fc1ce0c6b8d0a769a2142dfa4404bceeabe
Kim Tore Jensen <kimtjen@gmail.com>
2017-12-19 13:07:14 +0100

    make dropdown function return results based on input data

--- standalone/src/Dropdown.elm
+++ standalone/src/Dropdown.elm
@@ -5,11 +5,11 @@ import Html.Attributes exposing (..)
 import Model exposing (..)
 
 
+makeOption : Dokumentkategori -> Html msg
+makeOption d =
+    option [] [ text d ]
+
+
 dropdown : List Dokumentkategori -> Html msg
 dropdown dokumentkategoriList =
-    select [ id "InsuranceProviderId" ]
-        [ option [ value "1" ] [ text "Carrier 1" ]
-        , option [ value "2" ] [ text "Carrier 2" ]
-        , option [ value "3" ] [ text "Carrier 3" ]
-        , option [ value "4" ] [ text "Carrier 4" ]
-        ]
+    select [] (List.map makeOption dokumentkategoriList)
